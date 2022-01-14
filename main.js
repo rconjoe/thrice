c313c1867ce7315faff5d2b91c922711a3e512f6
rconjoe <joe@rconjoe.com>
2022-01-13 17:08:49 -0800

    init geometry - basic/standard torus - point/ambient light - light/grid helpers

--- main.js
+++ main.js
@@ -19,4 +19,32 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-renderer.render( scene, camera )
+renderer.render( scene, camera );
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
+const torus = new THREE.Mesh( geometry, material );
+
+scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(20,20,20)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(lightHelper, gridHelper)
+
+function animate() {
+  requestAnimationFrame( animate )
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  renderer.render( scene, camera )
+}
+
+animate()
